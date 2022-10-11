9941bd1b7ea7e8d3e3fa5b64ae8e402b60d83d49
Sayd-Ali Orzamiev <obkh1309@mail.ru>
2022-10-11 16:18:33 +0300

    sidebar start

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import {Route, Routes, Link} from 'react-router-dom';
-import {Suspense} from 'react';
 import {useTheme} from 'app/providers/ThemeProvider';
 import {classNames} from 'shared/lib/classNames/classNames';
 
 import './styles/index.scss';
-import {MainPage} from 'pages/MainPage';
-import {AboutPage} from 'pages/AboutPage';
 import {AppRouter} from 'app/providers/router';
+import {Navbar} from 'widgets/Navbar';
+import {Sidebar} from 'widgets/Sidebar';
 
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app',{},[theme])}>
-            <button onClick={toggleTheme}>Toggle</button>
-            <Link to='/'>Main</Link>
-            <Link to='/about'>About</Link>
-            <AppRouter />
+            <Navbar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
         </div>
     );
 };

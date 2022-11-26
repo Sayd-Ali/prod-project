794d3bb88f2ae8161ee2691c57bd16545b7c03cf
Sayd-Ali Orzamiev <obkh1309@mail.ru>
2022-11-26 19:25:26 +0300

    aaa

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     plugins: [
         'react',
         '@typescript-eslint',
-        'i18next',
+        'i18next ',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
